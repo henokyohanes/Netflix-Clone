3cd9b89b083c9f7decfe7ceb03740de08a609cc9
henokyohanesdev <henokyohanes8@gmail.com>
2024-10-31 00:18:51 -0500

    footer component with fully responsive styling and functionality implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fontsource/montserrat";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
