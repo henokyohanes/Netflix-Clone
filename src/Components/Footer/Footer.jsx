cfed70a553fbbeb5852bafbdaf276e2e3692a857
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-01 21:52:29 -0500

    styling updated on header and banner components.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,41 +7,41 @@ import "./Footer.css"
 
 const Footer = () => {
   return (
-    <div className="footer_container">
+    <div className="footer-container">
       <div className="footer">
-        <div className="footer_icons">
-          <a href="https://www.facebook.com/Netflix"> <FacebookIcon /> </a>
-          <a href="https://www.instagram.com/netflix/"> <InstagramIcon /> </a>
-          <a href="https://twitter.com/netflix"> <TwitterIcon /> </a>
-          <a href="https://www.youtube.com/netflix"> <YouTubeIcon /> </a>
+        <div className="footer-icons">
+          <a href="#"> <FacebookIcon /> </a>
+          <a href="#"> <InstagramIcon /> </a>
+          <a href="#"> <TwitterIcon /> </a>
+          <a href="#"> <YouTubeIcon /> </a>
         </div>
-        <div className="footer_ul">
+        <div className="footer-ul">
           <ul>
-            <li> <a href="https://www.netflix.com/browse/audio-description">Audio Description</a> </li>
-            <li> <a href="https://ir.netflix.net/ir-overview/profile/default.aspx">Investor Relations</a> </li>
-            <li> <a href="https://help.netflix.com/legal/privacy">privacy</a> </li>
-            <li> <a href="https://help.netflix.com/en/contactus">contcat us</a> </li>
+            <li> <a href="#">Audio Description</a> </li>
+            <li> <a href="#">Investor Relations</a> </li>
+            <li> <a href="#">privacy</a> </li>
+            <li> <a href="#">contcat us</a> </li>
           </ul>
           <ul>
-            <li> <a href="https://help.netflix.com/en">Help Center</a> </li>
-            <li> <a href="https://jobs.netflix.com/">Jobs</a> </li>
-            <li> <a href="https://help.netflix.com/legal/notices">Legal Notices</a> </li>
+            <li> <a href="#">Help Center</a> </li>
+            <li> <a href="#">Jobs</a> </li>
+            <li> <a href="#">Legal Notices</a> </li>
             <li>
-              <a href="https://help.netflix.com/legal/dnsspi">
+              <a href="#">
                 DO Not Sell or Share My Personal <br/> Information
               </a>
             </li>
           </ul>
-          <ul className='gift'>
-            <li> <a href="https://www.netflix.com/redeem">Gift Cards</a> </li>
-            <li> <a href="https://www.netflix.shop/">Netflix shop</a> </li>
+          <ul className="gift">
+            <li> <a href="#">Gift Cards</a> </li>
+            <li> <a href="#">Netflix shop</a> </li>
             <li> <a href="#">Cookie Preferences</a> </li>
-            <li> <a href="https://help.netflix.com/legal/dnsspi#DAA">Ad Choices</a> </li>
+            <li> <a href="#">Ad Choices</a> </li>
           </ul>
           <ul>
-            <li> <a href="https://media.netflix.com/en/">Media Center</a> </li>
-            <li> <a href="https://help.netflix.com/legal/termsofuse">Terms of Use</a> </li>
-            <li> <a href="https://help.netflix.com/en/node/134094">Corporate Information</a> </li>
+            <li> <a href="#">Media Center</a> </li>
+            <li> <a href="#">Terms of Use</a> </li>
+            <li> <a href="#">Corporate Information</a> </li>
           </ul>
         </div>
         <button className="serviceCode"> <a className="anchor" href="#">Service Code</a> </button>
